de8c757e560a3c8cd8e19e1572093591bb01ffe9
kis0522 <kiminsu0522@naver.com>
2024-04-05 11:50:23 +0900

    ch5

--- quiz/05-1/js/app.js
+++ quiz/05-1/js/app.js
@@ -4,14 +4,15 @@ var app = new Vue({
     message : 'Hello Vue.js',
     // 할 일 #1
     // 새로운 데이터 속성을 1개 추가하고, 머스타쉬 {{ }} 데이터 바인딩을 이용하여 화면에 표시해보세요.
+    secondMessage: 'Pain past is pleasure',
 
-    uid: 'id10',
     // 할 일 #2
     // uid를 변경하고 해당 uid의 변경 여부를 크롬 개발자 도구의 '화면 요소 검사 기능'으로 확인해보세요. p 태그의 id 값을 검사.
+    uid: 'id10',
 
-    flag: true
     // 할 일 #4
     // 위 flag 값을 false로 변경했을 때 화면에 어떤 영향을 주는지 확인해보세요.
+    flag: false
   },
   methods: {
     // ES6 문법
@@ -25,5 +26,8 @@ var app = new Vue({
 
     // 할 일 #3
     // eventMethod를 하나 추가하고 index.html의 #3 영역에 해당 이벤트를 실행하는 버튼을 추가하세요.
+    clickBtn2() {
+      console.log("btn2 is clicked");
+    }
   }
 });
